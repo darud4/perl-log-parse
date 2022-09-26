b0b1ff7580dfc01a07e3467525ed761a1b2097e3
Pavel Bezrukov <darud4@yandex.ru>
2022-09-26 23:54:34 +0400

    feat: добавить чтение конфигурации, заменить функции вставки строк на одну универсальную

--- parse.pl
+++ parse.pl
@@ -1,52 +1,76 @@
 use strict;
+use DBI;
 
-my $dbh = 0;
+my $config = readConfig();
 
-my $NO_ID_TO_LOG = 0;
-
+my $dbh = DBI->connect("dbi:Pg:dbname=logparser", 'logparseruser', 'password4log', {AutoCommit => 0});
 my $filename = shift || 'out';
+
 open INF, $filename;
-open MESSAGE_ERR, ">>$filename.message.err";
 open MESSAGE_INS, ">>$filename.message.ins";
-open LOG_ERR, ">>$filename.log.err";
 open LOG_INS, ">>$filename.log.ins";
+open ERR, ">>$filename.err";
 
 while (<INF>) {
   chomp;
-  my ($date, $time, $messageId, $flag, $email, $rest) = split(/\s+/, $_, 6);
-  my $id = undef;
-  if ($flag eq "<=") {
-    if ($rest =~ /\bid=(\S+)\b/) {
-       writeMessage({
-          timestamp => "$date $time", 
-          messageId => $messageId, 
-          id => ($id = $1), 
-          message => "$messageId $flag $email $rest"
-         });
-    }
+  unless (/^\s*(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(.*)$/) {
+    print ERR "No timestamp found: $_\n";
+    next;
   }
-  unless ($id) {writeLog(
-    {timestamp => "$date $time",
-     messageId => $messageId,
-     message => "$messageId $flag $email $rest",
-     address => $email
-    }
-  );}
+  
+  my $timestamp = $1;
+  my $messageBody = $2;
+  my ($messageId, $flag, $email, $rest) = split(/\s+/, $messageBody, 4);
+  my $id;
+  if ($flag eq "<=" && $rest =~ /\bid=(\S+)\b/) { 
+    print MESSAGE_INS makeInsert('messages', 
+      {
+        timestamp => $timestamp, 
+        messageId => $messageId, 
+        id => ($id = $1), 
+        message => $messageBody
+      });
+  }
+  unless ($id) {
+    print LOG_INS makeInsert('log',
+      {
+        timestamp => $timestamp,
+        messageId => $messageId,
+        message => $messageBody,
+        address => $email
+      }
+    );
+  }
+
 }
 close INF;
+close ERR;
+close MESSAGE_INS;
+close LOG_INS;
 
-sub writeMessage {
+sub makeInsert {
+  my $tableName = shift;
   my $payload = shift;
-
-  my $insert = "insert into messages (created, id, int_id, str) values ("
-    .$payload->{timestamp}.", $payload->{id}, $payload->{messageId}, $payload->{message})";
-  print MESSAGE_INS $insert."\n";
+  my $fields = join(",", keys %$payload);
+  my @placeholders = map { ":$_" } keys %$payload;
+  my $values = join (",", values %$payload);
+  my $insert = "insert into $tableName ($fields) values (".join (',', @placeholders).")";
+#  my $insert = "insert into $tableName ($fields) values ($values)";
+  return $insert;
 }
 
-sub writeLog {
-  my $payload = shift;
-
-  my $insert = "insert into log (created, int_id, str, address) values ("
-    .$payload->{timestamp}.", $payload->{messageId}, $payload->{message}, $payload->{address})";
-  print LOG_INS $insert."\n";
+sub readConfig {
+  open CONFIG, 'db.conf';
+  my %config;
+  while (<CONFIG>) {
+    chomp;                  
+    s/#.*//;                
+    s/^\s+//;               
+    s/\s+$//;               
+    next unless length;     
+    my ($key, $value) = split(/\s*=\s*/, $_, 2);
+    $config{$key} = $value;
+  }
+  close CONFIG;
+  return \%config;
 }
