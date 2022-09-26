b5eff5cc5770a34e5474a6e81a001225f6cfa250
Pavel Bezrukov <darud4@yandex.ru>
2022-09-26 19:27:20 +0400

    feat: Добавить функции добавления записей в таблицы сообщений и логов

--- parse.pl
+++ parse.pl
@@ -1,10 +1,52 @@
 use strict;
-my $filename = shift || die "No file specified, exiting...";
 
+my $dbh = 0;
+
+my $NO_ID_TO_LOG = 0;
+
+my $filename = shift || 'out';
 open INF, $filename;
+open MESSAGE_ERR, ">>$filename.message.err";
+open MESSAGE_INS, ">>$filename.message.ins";
+open LOG_ERR, ">>$filename.log.err";
+open LOG_INS, ">>$filename.log.ins";
+
 while (<INF>) {
   chomp;
-  my ($date, $time, $id, $flag, $email, @rest) = split(/\s+/);
-  
+  my ($date, $time, $messageId, $flag, $email, $rest) = split(/\s+/, $_, 6);
+  my $id = undef;
+  if ($flag eq "<=") {
+    if ($rest =~ /\bid=(\S+)\b/) {
+       writeMessage({
+          timestamp => "$date $time", 
+          messageId => $messageId, 
+          id => ($id = $1), 
+          message => "$messageId $flag $email $rest"
+         });
+    }
+  }
+  unless ($id) {writeLog(
+    {timestamp => "$date $time",
+     messageId => $messageId,
+     message => "$messageId $flag $email $rest",
+     address => $email
+    }
+  );}
 }
 close INF;
+
+sub writeMessage {
+  my $payload = shift;
+
+  my $insert = "insert into messages (created, id, int_id, str) values ("
+    .$payload->{timestamp}.", $payload->{id}, $payload->{messageId}, $payload->{message})";
+  print MESSAGE_INS $insert."\n";
+}
+
+sub writeLog {
+  my $payload = shift;
+
+  my $insert = "insert into log (created, int_id, str, address) values ("
+    .$payload->{timestamp}.", $payload->{messageId}, $payload->{message}, $payload->{address})";
+  print LOG_INS $insert."\n";
+}
