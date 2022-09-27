5e702a7c0927d9eb01631a28bc2bad840e404e49
Pavel Bezrukov <darud4@yandex.ru>
2022-09-27 22:50:21 +0400

    feat: добавить немного обработки ошибок и параметров

--- parse.pl
+++ parse.pl
@@ -1,20 +1,54 @@
+#!/usr/bin/perl
 use strict;
-use warnings;
+#use warnings;
 use DBI;
 
+# Если 1, то сообщения с флагом <=, у которых не найдена подстрока id=...., пишутся в таблицу log
+# В этом случае сумма записей в двух таблицах будет равна количеству строк в файле, и каждая запись из файла попадет в одну из таблиц
+# Если 0, то такие сообщения не пишутся никуда (в таблицу MESSAGE они не попадают, 
+# так как поле id в ней обязательное, а у них его нет, а в таблицу LOG не попадают, 
+# так как по условию записи с флагом "<=" в нее попадать не должны)
+my $WRITE_NO_ID_MESSAGES_TO_LOG = 0;
+
+# Если 1, то очищаем таблицы перед заливкой данных, если 0, то нет
+my $PURGE_BEFORE_INSERT = 1;
+
+# Имя файла лога по умолчанию
+my $DEFAULT_LOG_FILE = 'out';
+
+print "Reading configuration...";
 my $config = readConfig();
+print "done!\n";
 
+print "Connecting to database...";
 my $dbh = DBI->connect("dbi:Pg:dbname=$config->{database}", $config->{username}, $config->{password}, 
-  {AutoCommit => 0, RaiseError => 0});
-my $filename = shift || 'out';
+  {AutoCommit => 0, RaiseError => 0, PrintError => 0});
+if ($dbh) {
+  print "done!\n";
+} else {
+  print "ERROR!\n";
+  exit;
+}  
 
+my $filename = shift || $DEFAULT_LOG_FILE;
+
+print "Reading file $filename...\n";
 open INF, $filename;
 open MESSAGE_ERR, ">$filename.message.err";
 open LOG_ERR, ">$filename.log.err";
 open ERR, ">$filename.err";
 
+my ($recordsAll, $recordsMessage, $recordsLog, $recordsError) = (0, 0, 0, 0); 
+
+if ($PURGE_BEFORE_INSERT) {
+  print "Purging tables...\n";
+  purgeTable('message');
+  purgeTable('log');
+}
+
 while (<INF>) {
   chomp;
+  $recordsAll++;
   unless (/^\s*(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(.*)$/) {
     print ERR "No timestamp found: $_\n";
     next;
@@ -25,25 +59,41 @@ while (<INF>) {
   my ($messageId, $flag, $email, $rest) = split(/\s+/, $messageBody, 4);
   my $id;
   if ($flag eq "<=" && $rest =~ /\bid=(\S+)\b/) { 
-    makeInsert('message', 
+    $recordsMessage++;
+    my $result = makeInsert('message', 
       { created => $timestamp, int_id => $messageId, id => ($id = $1), str => $messageBody });
-    if ($dbh->err) {print MESSAGE_ERR "Error inserting in message!\n";}  
+    if (!$result) {print MESSAGE_ERR "Error inserting in message!".$dbh->errstr."\n$_\n";}  
   }
-  unless ($id) {
-    makeInsert('log',
+  if (($WRITE_NO_ID_MESSAGES_TO_LOG && !$id && $flag eq "<=") || ($flag ne '<=')) {
+    $recordsLog++;
+    my $result = makeInsert('log',
       { created => $timestamp, int_id => $messageId, str => $messageBody, address => $email });
-    if ($dbh->err) {print LOG_ERR "Error inserting in log!\n";}  
+    if (!$result) {print LOG_ERR "Error inserting in log!".$dbh->errstr."\n$_\n";}  
   }
-
+  if ($dbh->err) {$recordsError++; print ERR $dbh->errstr."\n$_\n"; };
 }
 close INF;
 close ERR;
 close MESSAGE_ERR;
 close LOG_ERR;
+
+print "File processed, lines: $recordsAll\n";
+print "Messages: $recordsMessage\n";
+print "Logs: $recordsLog\n";
+print "Errors: $recordsError\n";
+
 $dbh->commit;
 $dbh->disconnect;
+print "Done!\n";
+
+sub purgeTable {
+  my $tableName = shift;
+  my $delete = "delete from $tableName";
+  my $sth = $dbh->do($delete);
+}
 
 sub makeInsert {
+  my $result;
   my $tableName = shift;
   my $payload = shift;
   my @fields = keys %$payload;
@@ -53,8 +103,7 @@ sub makeInsert {
   for (my $i=0; $i < scalar(@fields); $i++) {
     $sth->bind_param($placeholders[$i], $payload->{$fields[$i]});
   }
-  $sth->execute();
-  return $insert;
+  return $sth->execute();
 }
 
 sub readConfig {
